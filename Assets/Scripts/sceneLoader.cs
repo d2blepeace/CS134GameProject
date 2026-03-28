53ed684ea1801186d1dab40990bfe071cfbbaf03
mothMountain <154105076+mothMountain@users.noreply.github.com>
2026-03-28 16:40:26 -0700

    Made Main Menu

---
+++ Assets/Scripts/sceneLoader.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class sceneLoader : MonoBehaviour
+{
+    public void LoadByName(string n)
+    {
+        SceneManager.LoadScene(n);
+    }
+}
