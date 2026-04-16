5cbbc0e02db386fd94d73ca75e10cde9bb31f062
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2026-04-15 17:40:17 -0700

    Added Resume Button and save progress feature

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -7,14 +7,41 @@ public class MainMenuUI : MonoBehaviour
 {
     [Header("Scene Name")]
     [SerializeField] private string gameSceneName = "Level_1"; // change to playing scene name 
+    [Header("For Resume Button")]
+    [SerializeField] private GameObject resumeButton;
 
+    void Start()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        bool hasResume = PlayerPrefs.GetInt("HasResume", 0) == 1;
+
+        if (resumeButton != null)
+            resumeButton.SetActive(hasResume);
+    }
     // Start is called before the first frame update
     public void StartGame()
     {
         Time.timeScale = 1f;
+
+        // Start new game from Level_1
+        PlayerPrefs.SetString("ResumeScene", gameSceneName);
+        PlayerPrefs.SetInt("HasResume", 1);
+        PlayerPrefs.Save();
+
         SceneManager.LoadScene(gameSceneName);
     }
 
+    //Resume game
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+
+        string resumeScene = PlayerPrefs.GetString("ResumeScene", gameSceneName);
+        SceneManager.LoadScene(resumeScene);
+    }
     // Settings button
     public void OpenSetting()
     {
@@ -28,5 +55,4 @@ public class MainMenuUI : MonoBehaviour
         Debug.Log("Quitting game...");
         Application.Quit();
     }
-
 }
