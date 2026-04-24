5d1751bb21f7cf95a469500786767893ea5d1d74
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2026-04-24 00:46:35 -0700

    Adjust mainMenu behavior of Resume button

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -10,36 +10,58 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private GameObject resumeButton;
 
     private void Start()
-    {
+    {   
         Time.timeScale = 1f;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
-        // Fresh start of game, HasResume = 0, only played once hasResume = 1
         bool hasResume = PlayerPrefs.GetInt("HasResume", 0) == 1;
+        string resumeScene = PlayerPrefs.GetString("ResumeScene", "");
 
         if (resumeButton != null)
-            resumeButton.SetActive(hasResume);
+            resumeButton.SetActive(hasResume && !string.IsNullOrEmpty(resumeScene));
     }
 
     public void StartGame()
     {
+
         Time.timeScale = 1f;
 
-        // Start from beginning
-        PlayerPrefs.SetString("ResumeScene", firstLevelSceneName);
-        PlayerPrefs.SetInt("HasResume", 1);
+        // New game should NOT create resume progress
+        PlayerPrefs.DeleteKey("HasResume");
+        PlayerPrefs.DeleteKey("ResumeScene");
         PlayerPrefs.Save();
 
         SceneManager.LoadScene(firstLevelSceneName);
+    }
+    
+    public void ClearSave()
+    {
+        PlayerPrefs.DeleteKey("HasResume");
+        PlayerPrefs.DeleteKey("ResumeScene");
+        PlayerPrefs.Save();
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1f;
 
-        string resumeScene = PlayerPrefs.GetString("ResumeScene", firstLevelSceneName);
-        SceneManager.LoadScene(resumeScene);
+        string resumeScene = PlayerPrefs.GetString("ResumeScene", "");
+
+        if (!string.IsNullOrEmpty(resumeScene))
+        {
+            SceneManager.LoadScene(resumeScene);
+        }
+        else
+        {
+            SceneManager.LoadScene(firstLevelSceneName);
+        }
+    }
+    public void SaveResumeScene(string sceneName)
+    {
+        PlayerPrefs.SetString("ResumeScene", sceneName);
+        PlayerPrefs.SetInt("HasResume", 1);
+        PlayerPrefs.Save();
     }
 
     public void OpenSetting()
@@ -52,4 +74,5 @@ public class MainMenuUI : MonoBehaviour
         Debug.Log("Quitting game...");
         Application.Quit();
     }
+
 }
